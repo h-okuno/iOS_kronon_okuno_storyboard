1a2029f9330ac18a908aeb7a6c0b4f47344097a5
h-okuno <hirokki.oku@gmail.com>
2021-02-17 09:52:48 +0900

    Initial Commit

---
+++ iOS_kronon_okuno_storyboard/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  iOS_kronon_okuno_storyboard
+//
+//  Created by 奥野博貴 on 2021/02/17.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
